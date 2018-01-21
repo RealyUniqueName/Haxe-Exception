d837623c4b8b883dab163f0bbcf21c3bd64d3eed
Alexander Kuzmenko <alex@stablex.ru>
2018-01-22 01:14:28 +0300

    debug

--- test/cases/TestStack.hx
+++ test/cases/TestStack.hx
@@ -6,7 +6,10 @@ import haxe.Exception;
 class TestStack extends BaseCase {
 	public function testSubtract() {
 		var e = deeperStack();
+		trace(e.stack);
+		trace('=======');
 		var result = e.stack.subtract(new Exception('error', e).stack);
+		trace(result);
 		Assert.equals(2, result.length);
 	}
 
